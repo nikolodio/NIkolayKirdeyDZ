5329e00848acfee946201d6a0e721ec0023af4a6
Nikolas White <nikolodio.k@gmail.com>
2023-06-23 13:58:48 +0300

    code without pitfalls

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter the first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a < b)
+{
+    Console.Write("max number: " + b);
+}
+else
+{
+    Console.Write("max number: " + a);
+}
